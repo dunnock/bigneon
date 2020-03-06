d5f2c7952e95334d6571e24423a415f99004e93c
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2020-03-06 11:57:29 +0200

    create-db script auto clone ../bn-bd

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -24,6 +24,10 @@ DB_URL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/$DBNAME"
 docker-compose up -d pg
 
 echo "Setting up $DB_URL via bndb-cli"
+if [[ ! -d $BN_DB_PATH ]]; then 
+  echo "bn-db not found, cloning to $BN_DB_PATH"
+  ./scripts/clone.sh bn-db
+fi
 cargo install --path $BN_DB_PATH --force
 if [ "$1" == "migrate" ]; then 
   bndb_cli migrate -c $DB_URL
